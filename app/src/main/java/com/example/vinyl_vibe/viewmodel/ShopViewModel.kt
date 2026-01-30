250e2b0496adef479fcaa62eadd2498d61eee9a7
REXARII <rexarii.dev@gmail.com>
2026-01-30 10:28:49 -0300

    creamos funciones de crear, eliminar, editar

--- app/src/main/java/com/example/vinyl_vibe/viewmodel/ShopViewModel.kt
+++ app/src/main/java/com/example/vinyl_vibe/viewmodel/ShopViewModel.kt
@@ -41,10 +41,59 @@ class ShopViewModel : ViewModel() {
         }
     }
 
-    // --- Funciones del Carrito (igual que antes) ---
+    // Funciones del Carrito
     fun addToCart(vinyl: Vinyl) { _cartItems.add(vinyl) }
     fun removeFromCart(vinyl: Vinyl) { _cartItems.remove(vinyl) }
     fun clearCart() { _cartItems.clear() }
     fun calculateTotal(): Int { return _cartItems.sumOf { it.precio } }
+
+    // nuevas funciones de agregar, eliminar y editar productos
+
+    // 1. AGREGAR PRODUCTO (Create)
+    fun agregarProducto(vinyl: Vinyl) {
+        viewModelScope.launch {
+            try {
+                Log.d("API_SHOP", "Intentando crear: ${vinyl.titulo}")
+                // Llamamos a la API
+                RetrofitClient.productService.crearProducto(vinyl)
+                // Si funciona, recargamos la lista para ver el nuevo disco
+                fetchProductos()
+            } catch (e: Exception) {
+                Log.e("API_SHOP", "Error al crear: ${e.message}")
+            }
+        }
+    }
+
+    // 2. ELIMINAR PRODUCTO (Delete)
+    fun eliminarProducto(id: Int) {
+        viewModelScope.launch {
+            try {
+                Log.d("API_SHOP", "Eliminando ID: $id")
+                // Llamamos a la API
+                RetrofitClient.productService.eliminarProducto(id)
+                // Recargamos la lista
+                fetchProductos()
+            } catch (e: Exception) {
+                Log.e("API_SHOP", "Error al eliminar: ${e.message}")
+            }
+        }
+    }
+
+    // 3. EDITAR PRODUCTO (Update)
+    fun editarProducto(id: Int, vinyl: Vinyl) {
+        viewModelScope.launch {
+            try {
+                Log.d("API_SHOP", "Editando ID: $id")
+                // Llamamos a la API
+                RetrofitClient.productService.editarProducto(id, vinyl)
+                // Recargamos la lista
+                fetchProductos()
+            } catch (e: Exception) {
+                Log.e("API_SHOP", "Error al editar: ${e.message}")
+            }
+        }
+    }
+
+
 }
 

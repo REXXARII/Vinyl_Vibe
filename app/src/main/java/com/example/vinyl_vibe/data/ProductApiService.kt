33989f043960036aca2f9347786b7ffa2e488e6c
REXARII <rexarii.dev@gmail.com>
2026-01-30 10:23:24 -0300

    creamos bon de crear, eliminar, editar

--- app/src/main/java/com/example/vinyl_vibe/data/ProductApiService.kt
+++ app/src/main/java/com/example/vinyl_vibe/data/ProductApiService.kt
@@ -2,7 +2,12 @@ package com.example.vinyl_vibe.data
 
 import com.example.vinyl_vibe.model.Vinyl
 import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface ProductApiService {
 
@@ -10,5 +15,17 @@ interface ProductApiService {
     // El servidor responde en la ruta "/productos"
     @GET("api/productos")
     suspend fun getProductos(): Response<List<Vinyl>>
+
+    // crear, eliminar, editar
+
+    @POST("productos")
+    suspend fun crearProducto(@Body vinyl: Vinyl): Vinyl
+
+    @DELETE("productos/{id}")
+    suspend fun eliminarProducto(@Path("id") id: Int)
+
+    @PUT("productos/{id}")
+    suspend fun editarProducto(@Path("id") id: Int, @Body vinyl: Vinyl): Vinyl
+
 }
 
